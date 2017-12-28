50a1d6b8265c0740bc1805a42b8042c5d5a266a2
Pavel Shvarchov <shvarpa@gmail.com>
2017-12-28 21:49:54 +0200

    updated cli

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='BibFast',
+    version='0.1',
+    packages=find_packages(exclude=['tests']),
+    url='https://github.com/Shvarpa/BibFast',
+    license='',
+    author='Shvarpa',
+    author_email='Shvarpa@gmail.com',
+    description='citation firebase database',
+    install_requires=['requests==2.11.1',
+                      'gcloud==0.17.0',
+                      'oauth2client==3.0.0',
+                      'requests_toolbelt==0.7.0',
+                      'python_jwt==2.0.1',
+                      'pycryptodome==3.4.3'
+                      ]
+)
